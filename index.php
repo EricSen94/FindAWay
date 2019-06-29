3621f5d58de407812ac9a859ac68690d43fc298f
laloses <46451243+Laloses@users.noreply.github.com>
2019-06-29 13:53:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
   <head>
     <!-- Required meta tags -->
     <meta name="viewport" http-equiv="Content-Type" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes; charset=utf-8">
-    <!-- Personal JS -->
-    <script src="scripts/vistaContenido.js" type="text/javascript"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Personal CSS -->
@@ -50,7 +48,7 @@
             <img class="imgBtn" src="img/nuevaB.png" >
         </button>
         <!---------------------------------------------Calificar -->
-        <button type="button" data-toggle="tooltip" data-placement="left" title="Calificar Ruta" class="boton bordes btn btn-outline-info">
+        <button type="button" data-toggle="tooltip" data-placement="left" title="Calificar Ruta" class="boton bordes btn btn-outline-info" onclick="calificar()">
             <img src="img/estrellaB.png" class="imgBtn">
         </button>
         <section class="redes bordes">
@@ -188,5 +186,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Personal JS -->
+    <script src="scripts/vistaContenido.js" type="text/javascript"></script>
   </body>
 </html>
