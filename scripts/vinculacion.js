b822ac2b438a97e6f46b6572b0c11fc2ae3d523b
laloses <46451243+Laloses@users.noreply.github.com>
2019-06-29 14:11:37 -0500

    Create vinculacion.js

---
+++ scripts/vinculacion.js
@@ -0,0 +1,9 @@
+//funcion para vincular
+function vincular(){
+//Primero vincular con google y guardar el nombre del usuario en una variable para después mandarlo a la base de datos
+}
+//funcion que califica una ruta
+function calificar(){
+//comprobar si ya está vinculado para abrir una pagina emergente y un menú de seleccion de ruta
+//Al dar clic en el boton de calificar se debe cerrar la ventana emergente y mandar la calificacion a la base de datos
+}
