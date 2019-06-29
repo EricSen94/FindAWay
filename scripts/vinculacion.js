ccfb71159d4ded7c85310d06612bc451e7022ff7
laloses <46451243+Laloses@users.noreply.github.com>
2019-06-29 14:48:46 -0500

    Update vinculacion.js

--- scripts/vinculacion.js
+++ scripts/vinculacion.js
@@ -1,9 +1,21 @@
 //funcion para vincular
 function vincular(){
 //Primero vincular con google y guardar el nombre del usuario en una variable para después mandarlo a la base de datos
+//return true;
 }
+
 //funcion que califica una ruta
 function calificar(){
-//comprobar si ya está vinculado para abrir una pagina emergente y un menú de seleccion de ruta
-//Al dar clic en el boton de calificar se debe cerrar la ventana emergente y mandar la calificacion a la base de datos
+//comprobar si ya está vinculado para abrir una pagina emergente y un menú para seleccionar la ruta a calificar
+//if(vincular()=true)
+//open window
+//Al dar clic en el boton de calificar se debe cerrar la ventana emergente y mandar calificacion
+//<botton title="Calificar" onclick="enviarCalificacion()">
+}
+
+function enviarCalificacion(){
+//sumar la calificacion haciendo un promedio entre 2 
+//calificacion a enviar = calificacion actual de la base de datos + calificacion mandada por el usuario / 2
+//enviarla a la base de datos
+//variableSQL = "enviar a calificiones $calificacionAEnviar WHERE id.ruta = rutaSeleccionada.id.ruta"
 }
