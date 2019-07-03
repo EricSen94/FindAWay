5cf1bd9120224771bca51beaf6d655e29a1fc9ce
laloses <46451243+Laloses@users.noreply.github.com>
2019-07-03 12:43:20 -0500

    Update Datos.php

--- scripts/Datos.php
+++ scripts/Datos.php
@@ -8,16 +8,17 @@ class Datos{
         $this->conexion = new conectarDB();
     }
     public function llenarTabla($lugar){
-        $consulta="SELECT rutas.nombre, rutas.descripcion, calificaciones.cant_estrellas FROM rutas LEFT JOIN calificaciones ON rutas.id_ruta=calificaciones.id_ruta WHERE rutas.nombre_destino = '".$lugar."' ORDER BY calificaciones.cant_estrellas DESC";
+        $consulta="SELECT rutas.nombre, rutas.descripcion, rutas.mapa, calificaciones.cant_estrellas FROM rutas LEFT JOIN calificaciones ON rutas.id_ruta=calificaciones.id_ruta WHERE rutas.nombre_destino = '".$lugar."' ORDER BY calificaciones.cant_estrellas DESC";
         
         $lRutas = $this->conexion->consultar($consulta);
         //Ciclo para imprimir n rutas acomodadas por mayor calificacion
         $n=0;
         while($arrayRutas = $lRutas->fetch_array(MYSQLI_ASSOC)){
-            $nomRuta = $arrayRutas['nombre'];
-            $descRuta = $arrayRutas['descripcion'];
+            $nomRuta = utf8_encode($arrayRutas['nombre']);
+            $descRuta = utf8_encode($arrayRutas['descripcion']);
+            $mapRuta = utf8_encode($arrayRutas['mapa']);
             $cantEstrellas = floatval($arrayRutas['cant_estrellas']);
-            $ref='<a class="nombreRuta fil-content" href="#Iruta" onclick="llenarInfo(\''.utf8_encode ($nomRuta).'\',\''.utf8_encode($descRuta).'\', loadMap)">';
+            $ref='<a class="nombreRuta fil-content" href="#Iruta" onclick="llenarInfo(\''.$nomRuta.'\',\''.$descRuta.'\',\''.$mapRuta.'\', loadMap)">';
             
             echo $ref."<div class='fil-col'><span>".$nomRuta."</span></div><div class='fil-col'><img src='img/estrellas/".$cantEstrellas.".png' class='imgE'></div></a>";
         }
